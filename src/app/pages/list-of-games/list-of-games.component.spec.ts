841d2d29e2724e6206823eaa07b699684e3c4238
Wilson Godoi <wilsonggodoi@gmail.com>
2020-04-01 18:15:14 -0300

    fixed all jasmine unit tests

--- src/app/pages/list-of-games/list-of-games.component.spec.ts
+++ src/app/pages/list-of-games/list-of-games.component.spec.ts
@@ -1,16 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ListOfGamesComponent } from './list-of-games.component';
+import { ListOfGamesComponent } from "./list-of-games.component";
+import { HttpClientModule } from "@angular/common/http";
+import { OrderByFieldPipe } from "src/app/resources/pipes/order-by-field.pipe";
 
-describe('ListOfGamesComponent', () => {
+describe("ListOfGamesComponent", () => {
   let component: ListOfGamesComponent;
   let fixture: ComponentFixture<ListOfGamesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListOfGamesComponent ]
-    })
-    .compileComponents();
+      declarations: [ListOfGamesComponent, OrderByFieldPipe],
+      imports: [HttpClientModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +21,7 @@ describe('ListOfGamesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
